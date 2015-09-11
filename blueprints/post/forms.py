f692f1c00d030f83824be317f46d1277b90bc334
Rahul Kumar <rahul@thoughtnirvana.com>
2015-09-12 02:09:23 +0530

    fix validators.

--- blueprints/post/forms.py
+++ blueprints/post/forms.py
@@ -1,15 +1,15 @@
-from flask.ext.wtf import Form, validators
+from flask.ext.wtf import Form
+from wtforms import validators
 from wtforms.ext.sqlalchemy.orm import model_form
 import models
 
-
-PostForm = model_form(models.Post, models.db.session, Form, field_args = {
+PostForm = model_form(models.Post, models.db.session, Form, field_args={
     'name': {'validators': [validators.Required()]},
     'title': {'validators': [validators.Required()]},
     'content': {'validators': [validators.Required()]},
 })
 
-CommentForm = model_form(models.Comment, models.db.session, Form, field_args = {
+CommentForm = model_form(models.Comment, models.db.session, Form, field_args={
     'commenter': {'validators': [validators.Required()]},
     'body': {'validators': [validators.Required()]},
 })
