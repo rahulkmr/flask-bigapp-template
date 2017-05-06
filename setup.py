263a5db8c6d9a1423b63c34bc41e2c316e57d29b
Rahul Kumar <rahul@thoughtnirvana.com>
2017-05-06 23:48:10 +0530

    Dependencies update.

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ environ = os.environ.get('FLASK_ENV')
 
 requirements = [
     # Flask
-    'Flask==0.10',
+    'Flask==0.12',
 
     # Flask extensions
-    'Flask-Sqlalchemy==2.0',
-    'Flask-Wtf==0.12',
-    'Flask-Babel==0.9',
+    'Flask-Sqlalchemy==2.1',
+    'Flask-Wtf==0.14',
+    'Flask-Babel==0.11.2',
     'Flask-Cache==0.13.1',
-    'Flask-Assets==0.11',
+    'Flask-Assets==0.12',
     'Flask-Script==2.0.5',
     'Flask-Bcrypt==0.6.2',
 
