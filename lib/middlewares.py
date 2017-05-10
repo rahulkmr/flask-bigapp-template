cc3a6200a420e635655fd2a571c36f12dc6aa2c8
Rahul Kumar <rahul@thoughtnirvana.com>
2017-05-10 15:34:58 +0530

    MethodRewriteMiddleware

--- lib/middlewares.py
+++ lib/middlewares.py
@@ -1,23 +1,26 @@
-from werkzeug import url_decode
+
 
 class MethodRewriteMiddleware(object):
-    """
-    Middleware to handle RESTful requests from clients which
-    don't support whole of REST (GET, PUT, POST, DELETE).
-
-    The method name is passed as a form field and the request
-    is re-written here.
-    """
+    allowed_methods = frozenset([
+        'GET',
+        'HEAD',
+        'POST',
+        'DELETE',
+        'PUT',
+        'PATCH',
+        'OPTIONS'
+    ])
+    bodyless_methods = frozenset(['GET', 'HEAD', 'OPTIONS', 'DELETE'])
 
     def __init__(self, app, input_name='_method'):
         self.app = app
         self.input_name = input_name
 
     def __call__(self, environ, start_response):
-        if self.input_name in environ.get('QUERY_STRING', ''):
-            args = url_decode(environ['QUERY_STRING'])
-            method = args.get(self.input_name).upper()
-            if method and method in ['GET', 'POST', 'PUT', 'DELETE']:
-                method = method.encode('ascii', 'replace')
-                environ['REQUEST_METHOD'] = method
+        method = environ.get(self.input_name, '').upper()
+        if method in self.allowed_methods:
+            method = method.encode('ascii', 'replace')
+            environ['REQUEST_METHOD'] = method
+        if method in self.bodyless_methods:
+            environ['CONTENT_LENGTH'] = '0'
         return self.app(environ, start_response)
