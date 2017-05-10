826f3c5d525956a1a6175a2a873b4b8eee225e5c
Rahul Kumar <rahul@thoughtnirvana.com>
2017-05-10 14:18:30 +0530

    Get rid of strict versions.

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ requirements = [
     'Flask==0.12',
 
     # Flask extensions
-    'Flask-Sqlalchemy==2.1',
-    'Flask-Wtf==0.14',
-    'Flask-Babel==0.11.2',
-    'Flask-Cache==0.13.1',
-    'Flask-Assets==0.12',
-    'Flask-Script==2.0.5',
-    'Flask-Bcrypt==0.6.2',
+    'Flask-Sqlalchemy',
+    'Flask-Wtf',
+    'Flask-Babel',
+    'Flask-Cache',
+    'Flask-Assets',
+    'Flask-Script',
+    'Flask-Bcrypt',
 
     # Misc 3rd party
     'augment==0.4',
